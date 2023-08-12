61355cff4ca436ca19135e1ed57830a8c4be3b6e
Hoàng Gia Anh <ngotthe0509@gmail.com>
2023-08-12 20:49:23 +0700

    upload source code

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('signup/', views.signup, name="signup"),
+    path('cart/', views.cart, name = "cart"),
+    path('checkout/', views.checkout, name = "checkout"),
+    path('update_item/', views.updateItem, name = "update_item"),
+]
